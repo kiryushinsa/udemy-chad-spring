35ff9fcb70c2af03af030b8ae63373afa7c42b16
kiryushinsa <kiryushin.s.a@gmail.com>
2021-12-12 15:54:23 +0300

    lesson85 - injection with Java annotations

---
+++ src/main/java/com/kiryushin/test/learn/lesson60/SportConfig.java
@@ -0,0 +1,9 @@
+package com.kiryushin.test.learn.lesson60;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kiryushin.test.learn.lesson60")
+public class SportConfig {
+}
