248a56ed4a3f0906e0e6781d9bf4c116a913f078
skiryushin <andeadushka@gmail.com>
2022-07-11 18:31:04 +0300

    Chapter 432-442. DB support.

--- src/main/java/com/kiryushinsa/springsecurity/config/DemoAppConfig.java
+++ src/main/java/com/kiryushinsa/springsecurity/config/DemoAppConfig.java
@@ -1,18 +1,32 @@
 package com.kiryushinsa.springsecurity.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.kiryushinsa.springsecurity")
+@PropertySource("classpath:postgres.properties")
 public class DemoAppConfig {
-    // define bean for ViewResolver
     
+    @Autowired
+    private Environment environment;
+    
+    private Logger logger = Logger.getLogger(getClass().getName());
+    
+    // define bean for ViewResolver
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -20,4 +34,39 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+    
+    @Bean
+    public DataSource securityDataSource() {
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+    
+        try {
+            securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        
+        logger.info(">>>> jdbc.url= " + environment.getProperty("jdbc.url"));
+        logger.info(">>>> jdbc.user= " + environment.getProperty("jdbc.user"));
+    
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+        
+        // set connection pool size
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+        
+        return securityDataSource;
+    }
+    
+    // helper method reads environment properties or convert in int
+    private int getIntProperty(String propertyName) {
+        String propertyValue = environment.getProperty(propertyName);
+    
+        assert propertyValue != null;
+        return Integer.parseInt(propertyValue);
+    }
+    
 }
