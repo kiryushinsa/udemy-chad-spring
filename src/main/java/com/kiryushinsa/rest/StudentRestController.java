7f17682899aecc34c78c439f745d2bb5ff6a9136
skiryushin <andeadushka@gmail.com>
2022-09-15 18:57:44 +0300

    Chapter 477-485. Exceptions. Handling REST exceptions.

--- src/main/java/com/kiryushinsa/rest/StudentRestController.java
+++ src/main/java/com/kiryushinsa/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.kiryushinsa.rest;
 
 import com.kiryushinsa.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -32,7 +31,15 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        // check the student id
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("StudentID not found - " + studentId );
+        }
+
         return students.get(studentId);
     }
+
+
 
 }
