ea0ceb950bcf4c59021815283ea6fa636c047cd8
skiryushin <andeadushka@gmail.com>
2022-10-18 19:29:45 +0300

    Chapter 513-514. Spring Boot. Initializing application.

---
+++ src/main/java/com/kiryushin/springboot/teachapp/TeachappApplication.java
@@ -0,0 +1,13 @@
+package com.kiryushin.springboot.teachapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiryushin/springboot/teachapp/TeachappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiryushin.springboot.teachapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
